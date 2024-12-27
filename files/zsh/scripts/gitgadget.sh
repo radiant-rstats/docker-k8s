c1ce66326c947c309da14b7a05f386be4fcc3cc4
vnijs <vnijs@ucsd.edu>
2024-12-26 20:46:33 -0800

    app cli

---
+++ files/zsh/scripts/gitgadget.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+R -e 'gitgadget:::gitgadget(host="0.0.0.0", port=8282, launch.browser=FALSE)' &
+sleep 2
+xdg-open http://localhost:8282
+wait $!
---
+++ files/zsh/scripts/radiant.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+R -e 'radiant.data:::launch(package="radiant", host="0.0.0.0", port=8181, run=FALSE)' &
+sleep 2
+xdg-open http://localhost:8181
+wait $!
