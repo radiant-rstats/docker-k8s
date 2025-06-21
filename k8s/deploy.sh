9ab64dc3ef0c026043584899abf6fbef7d6b2272
Vincent Nijs <vnijs@ucsd.edu>
2025-06-20 18:34:40 -0700

    update

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -4,6 +4,15 @@ k8s/start-pod.sh "-gpu"
 cat ~/.ssh/config
 clear
 
+# check issues on sc0 vs sc2 - big differences on 2/19
+# may be related to nvidia setup on sc0
+# if deactivating helps on sc0 then we can try to fix nvidia on sc0
+microk8s kubectl get pods -A --sort-by=.metadata.creationTimestamp
+microk8s kubectl logs -n kube-system calico-node-glj4l -c calico-node --tail=100
+
+microk8s kubectl get ippool default-ipv4-ippool -o yaml
+microk8s kubectl get felixconfiguration default -o yaml
+
 microk8s kubectl get pod
 microk8s kubectl get services
 microk8s inspect
@@ -43,6 +52,17 @@ microk8s stop
 
 # Start microk8s
 microk8s start
+sudo /snap/bin/microk8s reset
+
+# list all microk8s enabled add-ons
+microk8s add-ons list
+microk8s enable hostpath-storage
+microk8s enable nvidia
+
+microk8s kubectl get pods -A --sort-by=.metadata.creationTimestamp
+
+
+
 
 # Verify status
 microk8s status
